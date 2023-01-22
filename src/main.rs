65ea36a4776f05f446a7a5a659cb2fdf3c8343ab
Shaurya Goel <shaurya.goel@awiros.com>
2023-01-22 20:45:51 +0530

    add basic function to tuple struct

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,21 @@ mod tuple;
 use tuple::Tuple;
 
 fn main() {
-    let test_data = Tuple {
+    let data1 = Tuple {
         x: 1.0,
         y: 2.0,
         z: 3.0,
         w: 0.0,
     };
+
+    let data2 = Tuple {
+        x: 0.0,
+        y: 1.0,
+        z: 2.0,
+        w: 0.0,
+    };
+
+    let test_data: Tuple = data1.add(&data2);
 
     println!(
         "Tuple points are: x={}, y={}, z={}, w={}",
--- src/tuple.rs
+++ src/tuple.rs
@@ -4,3 +4,22 @@ pub struct Tuple {
     pub z: f32, // z coordinate of the Tuple
     pub w: f32, // indicator whether the Tuple is a vector(w=0.0) or a point(w=1.0). It is float as we need it for computation rather than an actual indicator
 }
+
+impl Tuple {
+    pub fn add(&self, other: &Tuple) -> Tuple {
+        Tuple {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        }
+    }
+
+    pub fn is_vector(&self) -> bool {
+        self.w == 0.0
+    }
+
+    pub fn is_point(&self) -> bool {
+        self.w == 1.0
+    }
+}
