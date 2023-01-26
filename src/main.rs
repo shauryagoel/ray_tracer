c7b942ec661aeb8b34cac77d96ca156527e89f0e
Shaurya Goel <shauryagoel0@gmail.com>
2023-01-26 19:11:03 +0530

    Implement override for add of Tuple
    Noticed that custom equality for float might be doing nothing, need to
    fix this

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let data1 = Tuple::new(1.0, 2.0, 3.0, 0.0);
     let data2 = Tuple::new(0.0, 1.0, 2.0, 0.0);
 
-    let test_data: Tuple = data1.add(&data2);
+    let test_data: Tuple = data1 + data2;
 
     println!(
         "Tuple points are: x={}, y={}, z={}, w={}",
