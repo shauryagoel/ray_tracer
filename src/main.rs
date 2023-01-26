1c95bead7ebd40cc673d23503beafe0f36039f61
Shaurya Goel <shauryagoel0@gmail.com>
2023-01-26 16:54:24 +0530

    use new function for tuple to avoid boilerplate when creating tuple of object

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,8 @@ mod tuple;
 use tuple::Tuple;
 
 fn main() {
-    let data1 = Tuple {
-        x: 1.0,
-        y: 2.0,
-        z: 3.0,
-        w: 0.0,
-    };
-
-    let data2 = Tuple {
-        x: 0.0,
-        y: 1.0,
-        z: 2.0,
-        w: 0.0,
-    };
+    let data1 = Tuple::new(1.0, 2.0, 3.0, 0.0);
+    let data2 = Tuple::new(0.0, 1.0, 2.0, 0.0);
 
     let test_data: Tuple = data1.add(&data2);
 
