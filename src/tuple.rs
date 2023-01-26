6bc15ec2da84abf0442e99e8009e029886bbf0b0
Shaurya Goel <shaurya.goel@awiros.com>
2023-01-26 16:54:24 +0530

    use new function for tuple to avoid boilerplate when creating tuple of object

--- src/tuple.rs
+++ src/tuple.rs
@@ -7,21 +7,25 @@ pub struct Tuple {
 
 // Factory methods for creating vector and point
 pub fn vector(x: f32, y: f32, z: f32) -> Tuple {
-    Tuple { x, y, z, w: 0.0 }
+    Tuple::new(x, y, z, 0.0)
 }
 
 pub fn point(x: f32, y: f32, z: f32) -> Tuple {
-    Tuple { x, y, z, w: 1.0 }
+    Tuple::new(x, y, z, 1.0)
 }
 
 impl Tuple {
+    pub fn new(x: f32, y: f32, z: f32, w: f32) -> Tuple {
+        Tuple { x, y, z, w }
+    }
+
     pub fn add(&self, other: &Tuple) -> Tuple {
-        Tuple {
-            x: self.x + other.x,
-            y: self.y + other.y,
-            z: self.z + other.z,
-            w: self.w + other.w,
-        }
+        Tuple::new(
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.z,
+            self.w + other.w,
+        )
     }
 
     pub fn is_vector(&self) -> bool {
@@ -39,12 +43,7 @@ mod tests {
 
     #[test]
     fn point_validity() {
-        let a = Tuple {
-            x: 4.3,
-            y: -4.2,
-            z: 3.1,
-            w: 1.0,
-        };
+        let a = Tuple::new(4.3, -4.2, 3.1, 1.0);
         assert_eq!(a.x, 4.3);
         assert_eq!(a.y, -4.2);
         assert_eq!(a.z, 3.1);
@@ -55,12 +54,7 @@ mod tests {
 
     #[test]
     fn vector_validity() {
-        let a = Tuple {
-            x: 4.3,
-            y: -4.2,
-            z: 3.1,
-            w: 0.0,
-        };
+        let a = Tuple::new(4.3, -4.2, 3.1, 0.0);
         assert_eq!(a.x, 4.3);
         assert_eq!(a.y, -4.2);
         assert_eq!(a.z, 3.1);
@@ -72,24 +66,14 @@ mod tests {
     #[test]
     fn point_factory() {
         let p = point(4.0, -4.0, 3.0);
-        let _p = Tuple {
-            x: 4.0,
-            y: -4.0,
-            z: 3.0,
-            w: 1.0,
-        };
-        assert!(p.is_point());
+        let _p = Tuple::new(4.0, -4.0, 3.0, 1.0);
+        assert!(p.is_point()); // TODO: compare p and _p
     }
 
     #[test]
     fn vector_factory() {
         let p = vector(4.0, -4.0, 3.0);
-        let _p = Tuple {
-            x: 4.0,
-            y: -4.0,
-            z: 3.0,
-            w: 0.0,
-        };
-        assert!(p.is_vector());
+        let _p = Tuple::new(4.0, -4.0, 3.0, 0.0);
+        assert!(p.is_vector()); // TODO: compare p and _p
     }
 }
