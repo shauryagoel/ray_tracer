69e67dccd8d20476a6a927f1a5120e2c1a0d790f
Shaurya Goel <shauryagoel0@gmail.com>
2023-01-27 15:20:58 +0530

    Add projectile and environment struct and tick function
    Need to move the demo code from main.rs.
    Need to update Projectile inplace in tick() instead of creating a new
    projectile every tick

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+mod projectile;
 mod tuple;
 mod utils;
-use tuple::Tuple;
+use projectile::{Environment, Projectile};
+use tuple::{point, vector, Tuple};
 
 fn main() {
     let data1 = Tuple::new(1.0, 2.0, 3.0, 0.0);
@@ -11,5 +13,36 @@ fn main() {
     println!(
         "Tuple points are: x={}, y={}, z={}, w={}",
         test_data.x, test_data.y, test_data.z, test_data.w
+    );
+
+    let starting_position = point(0.0, 1.0, 0.0);
+    let starting_velocity = vector(1.0, 1.0, 0.0).normalize();
+    let gravity = vector(0.0, -0.1, 0.0);
+    let wind = vector(-0.01, 0.0, 0.0);
+
+    let projectile = Projectile {
+        position: starting_position,
+        velocity: starting_velocity,
+    };
+    let mut environment = Environment {
+        gravity,
+        wind,
+        projectile,
+    };
+
+    let mut number_of_tick_to_hit_ground: u32 = 0;
+    while environment.projectile.position.y > 0.0 {
+        println!(
+            "Projectile position- x:{}, y:{}, z: {}",
+            environment.projectile.position.x,
+            environment.projectile.position.y,
+            environment.projectile.position.z
+        );
+        environment.projectile = environment.tick();
+        number_of_tick_to_hit_ground += 1;
+    }
+    println!(
+        "Number of ticks to hit the ground: {}",
+        number_of_tick_to_hit_ground
     );
 }
