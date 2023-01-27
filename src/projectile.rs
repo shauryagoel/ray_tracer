47ae4120954eb2b965bfa37ffd37e9ac6a67c402
Shaurya Goel <shaurya.goel@awiros.com>
2023-01-27 15:20:58 +0530

    Add projectile and environment struct and tick function
    Need to move the demo code from main.rs.
    Need to update Projectile inplace in tick() instead of creating a new
    projectile every tick

---
+++ src/projectile.rs
@@ -0,0 +1,23 @@
+use crate::tuple::Tuple;
+
+pub struct Projectile {
+    pub position: Tuple,
+    pub velocity: Tuple,
+}
+
+pub struct Environment {
+    pub gravity: Tuple,
+    pub wind: Tuple,
+    pub projectile: Projectile,
+}
+
+impl Environment {
+    pub fn tick(&mut self) -> Projectile {
+        let new_position = self.projectile.position + self.projectile.velocity;
+        let new_velocity = self.projectile.velocity + self.gravity + self.wind;
+        Projectile {
+            position: new_position,
+            velocity: new_velocity,
+        }
+    }
+}
