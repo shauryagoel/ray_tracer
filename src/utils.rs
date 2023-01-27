038fb0e211aecaef01a606725382d78fa6198b36
Shaurya Goel <shaurya.goel@awiros.com>
2023-01-27 11:38:28 +0530

    better support for custom equality comparison for f32

--- src/utils.rs
+++ src/utils.rs
@@ -1,33 +1,51 @@
 // Utility functions or structs or traits
 
-const EPSILON: f32 = 1e-5;
+const EPSILON: f32 = 1.0e-5;
 
 trait PartialEq {
-    fn eq(&self, other: &f32) -> bool;
-    fn ne(&self, other: &f32) -> bool;
+    fn eq(self, other: f32) -> bool;
+    fn neq(self, other: f32) -> bool;
 }
 
 impl PartialEq for f32 {
-    fn eq(&self, other: &f32) -> bool {
+    fn eq(self, other: f32) -> bool {
         (self - other).abs() < EPSILON
     }
 
-    fn ne(&self, other: &f32) -> bool {
-        (self - other).abs() >= EPSILON
+    fn neq(self, other: f32) -> bool {
+        !self.eq(other)
     }
 }
 
 #[cfg(test)]
-mod tests {
+mod utility_test {
     use super::*;
 
     #[test]
-    fn float_compare_equal() {
-        assert!(3.3 == 3.3);
+    fn float_compare_equal1() {
+        let a: f32 = 3.3;
+        let b: f32 = 3.3;
+        assert!(a.eq(b));
     }
 
     #[test]
-    fn float_compare_not_equal() {
-        assert!(3.3 != 3.2);
+    fn float_compare_equal2() {
+        let a: f32 = 0.0;
+        let b: f32 = 0.000005;
+        assert!(a.eq(b));
+    }
+
+    #[test]
+    fn float_compare_not_equal1() {
+        let a: f32 = 3.3;
+        let b: f32 = 3.2;
+        assert!(a.neq(b));
+    }
+
+    #[test]
+    fn float_compare_not_equal2() {
+        let a: f32 = 3.3;
+        let b: f32 = 3.2;
+        assert!(a.neq(b));
     }
 }
