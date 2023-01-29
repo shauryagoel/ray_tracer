67f2fae8225d62b441e8916e21a06e1b89844ed9
Shaurya Goel <shauryagoel0@gmail.com>
2023-01-29 16:53:49 +0530

    remove main.rs and instead do chapter wise binary compilation
    I think that is a better way going forward. We don't need package level
    binary, because first of all what should I put in it?? We can instead do
    separate compilation of multiple binaries when needed.
    Also I think we should make classes such as tuples, projectile into
    separate libraries.

--- src/bin/chapter1_end.rs
+++ src/bin/chapter1_end.rs
@@ -1,16 +1,6 @@
-use ray_tracer::{point, vector, Environment, Projectile, Tuple};
+use ray_tracer::{point, vector, Environment, Projectile};
 
 fn main() {
-    let data1 = Tuple::new(1.0, 2.0, 3.0, 0.0);
-    let data2 = Tuple::new(0.0, 1.0, 2.0, 0.0);
-
-    let test_data: Tuple = data1 + data2;
-
-    println!(
-        "Tuple points are: x={}, y={}, z={}, w={}",
-        test_data.x, test_data.y, test_data.z, test_data.w
-    );
-
     let starting_position = point(0.0, 1.0, 0.0);
     let starting_velocity = vector(1.0, 1.0, 0.0).normalize();
     let gravity = vector(0.0, -0.1, 0.0);
