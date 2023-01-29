bfe416fe61970a309acb8c1f2b6e390601b8e0a1
Shaurya Goel <shaurya.goel@awiros.com>
2023-01-29 18:27:07 +0530

    add color struct and its associated functions and tests

---
+++ src/color.rs
@@ -0,0 +1,149 @@
+use crate::Compare;
+
+// Color class to represent a color; similar structure to Tuple
+// All the components should be between 0 and 1
+// No such constraint has been added because during processing of algorithms, color value can be <0 or >1,
+// if we clip at every stage, then the final image would be too dark or too light
+pub struct Color {
+    pub red: f32,   // red component of the color
+    pub green: f32, // green component of the color
+    pub blue: f32,  // blue component of the color
+}
+
+impl Color {
+    pub fn new(red: f32, green: f32, blue: f32) -> Color {
+        Color { red, green, blue }
+    }
+
+    pub fn black() -> Color {
+        Color::new(0.0, 0.0, 0.0)
+    }
+
+    pub fn white() -> Color {
+        Color::new(1.0, 1.0, 1.0)
+    }
+
+    pub fn red() -> Color {
+        Color::new(1.0, 0.0, 0.0)
+    }
+
+    pub fn green() -> Color {
+        Color::new(0.0, 1.0, 0.0)
+    }
+
+    pub fn blue() -> Color {
+        Color::new(0.0, 0.0, 1.0)
+    }
+}
+
+impl std::ops::Add for Color {
+    type Output = Self;
+
+    fn add(self, other: Color) -> Self {
+        Color::new(
+            self.red + other.red,
+            self.green + other.green,
+            self.blue + other.blue,
+        )
+    }
+}
+
+impl std::ops::Sub for Color {
+    type Output = Self;
+
+    fn sub(self, other: Color) -> Self {
+        Color::new(
+            self.red - other.red,
+            self.green - other.green,
+            self.blue - other.blue,
+        )
+    }
+}
+
+impl PartialEq for Color {
+    fn eq(&self, other: &Color) -> bool {
+        self.red.eq(other.red) && self.green.eq(other.green) && self.blue.eq(other.blue)
+    }
+}
+
+// For color * a
+impl std::ops::Mul<f32> for Color {
+    type Output = Self;
+
+    fn mul(self, a: f32) -> Self {
+        Color::new(self.red * a, self.green * a, self.blue * a)
+    }
+}
+
+// For a * color
+impl std::ops::Mul<Color> for f32 {
+    type Output = Color;
+
+    fn mul(self, color: Color) -> Color {
+        Color::new(self * color.red, self * color.green, self * color.blue)
+    }
+}
+
+// For color1 * color2
+impl std::ops::Mul<Color> for Color {
+    type Output = Self;
+
+    fn mul(self, color: Self) -> Self {
+        Color::new(
+            self.red * color.red,
+            self.green * color.green,
+            self.blue * color.blue,
+        )
+    }
+}
+
+#[cfg(test)]
+mod color_tests {
+    use super::*;
+
+    #[test]
+    fn color_validity() {
+        let c = Color::new(-0.5, 0.4, 1.7);
+        assert_eq!(c.red, -0.5);
+        assert_eq!(c.green, 0.4);
+        assert_eq!(c.blue, 1.7);
+    }
+
+    #[test]
+    fn color_add() {
+        let c1 = Color::new(0.9, 0.6, 0.75);
+        let c2 = Color::new(0.7, 0.1, 0.25);
+        let _c = Color::new(1.6, 0.7, 1.0);
+        assert!(c1 + c2 == _c);
+    }
+
+    #[test]
+    fn color_sub() {
+        let c1 = Color::new(0.9, 0.6, 0.75);
+        let c2 = Color::new(0.7, 0.1, 0.25);
+        let _c = Color::new(0.2, 0.5, 0.5);
+        assert!(c1 - c2 == _c);
+    }
+
+    #[test]
+    fn color_scalar_multiply1() {
+        let c = Color::new(0.2, 0.3, 0.4);
+        let _c = Color::new(0.4, 0.6, 0.8);
+        assert!(2.0 * c == _c);
+    }
+
+    #[test]
+    fn color_scalar_multiply2() {
+        let c = Color::new(0.2, 0.3, 0.4);
+        let _c = Color::new(0.4, 0.6, 0.8);
+        assert!(c * 2.0 == _c);
+    }
+
+    #[test]
+    fn color_color_multiply() {
+        let c1 = Color::new(1.0, 0.2, 0.4);
+        let c2 = Color::new(0.9, 1.0, 0.1);
+        let _c = Color::new(0.9, 0.2, 0.04);
+        assert!(c1 * c2 == _c);
+    }
+}
