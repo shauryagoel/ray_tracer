9fa401b3aab03067fc1b51cb79b71863aacef925
Shaurya Goel <shauryagoel0@gmail.com>
2023-01-29 18:27:07 +0530

    add color struct and its associated functions and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+mod color;
 mod projectile;
 mod tuple;
 mod utils;
 
+pub use color::Color;
 pub use projectile::{Environment, Projectile};
 pub use tuple::{point, vector, Tuple};
 pub use utils::Compare;
