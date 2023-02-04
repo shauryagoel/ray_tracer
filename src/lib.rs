415c5aaaaf57d6b224d7bb409007f92793cdecd1
Shaurya Goel <shaurya.goel@awiros.com>
2023-02-04 19:13:59 +0530

    add basic 4x4 matrix struct and test
    I learned about arrays

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 mod canvas;
 mod color;
+mod matrix;
 mod projectile;
 mod tuple;
 mod utils;
 
 pub use canvas::Canvas;
 pub use color::Color;
+pub use matrix::Matrix;
 pub use projectile::{Environment, Projectile};
 pub use tuple::{point, vector, Tuple};
 pub use utils::Compare;
