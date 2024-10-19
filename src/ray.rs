b56698510076467bf01cc486f3e80d29336f93c9
Shaurya Goel <shaurya.goel@cars24.com>
2024-10-19 16:33:23 +0530

    Add ray transformation

--- src/ray.rs
+++ src/ray.rs
@@ -1,4 +1,4 @@
-use crate::Tuple;
+use crate::{Matrix, Tuple};
 
 #[derive(Debug)]
 pub struct Ray {
@@ -15,12 +15,20 @@ impl Ray {
     pub fn position(&self, t: f32) -> Tuple {
         self.origin + self.direction * t
     }
+
+    // Transform a ray by transforming individual components
+    pub fn transform(&self, m: Matrix) -> Self {
+        Self {
+            origin: m * self.origin,
+            direction: m * self.direction,
+        }
+    }
 }
 
 #[cfg(test)]
 mod ray_tests {
     use super::*;
-    use crate::{point, vector};
+    use crate::{point, vector, Matrix};
 
     #[test]
     fn ray_creation() {
@@ -40,5 +48,23 @@ mod ray_tests {
         assert_eq!(ray.position(1.0), point(3.0, 3.0, 4.0));
         assert_eq!(ray.position(-1.0), point(1.0, 3.0, 4.0));
         assert_eq!(ray.position(2.5), point(4.5, 3.0, 4.0));
+    }
+
+    #[test]
+    fn translation_ray() {
+        let r = Ray::new(point(1.0, 2.0, 3.0), vector(0.0, 1.0, 0.0));
+        let m = Matrix::get_translation_matrix(3.0, 4.0, 5.0);
+        let r2 = r.transform(m);
+        assert_eq!(r2.origin, point(4.0, 6.0, 8.0));
+        assert_eq!(r2.direction, vector(0.0, 1.0, 0.0));
+    }
+
+    #[test]
+    fn scaling_ray() {
+        let r = Ray::new(point(1.0, 2.0, 3.0), vector(0.0, 1.0, 0.0));
+        let m = Matrix::get_scaling_matrix(2.0, 3.0, 4.0);
+        let r2 = r.transform(m);
+        assert_eq!(r2.origin, point(2.0, 6.0, 12.0));
+        assert_eq!(r2.direction, vector(0.0, 3.0, 0.0));
     }
 }
