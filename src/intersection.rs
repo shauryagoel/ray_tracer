4247a6b7448dcbc16ccbb987d254019a3565b968
Shaurya Goel <shaurya.goel@cars24.com>
2024-10-19 16:57:51 +0530

    Add ray intersects transformed sphere

--- src/intersection.rs
+++ src/intersection.rs
@@ -110,7 +110,7 @@ mod sphere_tests {
     fn intersection_returning_objects() {
         let r = Ray::new(point(0.0, 0.0, -5.0), vector(0.0, 0.0, 1.0));
         let s: Sphere = Default::default();
-        let xs = s.intersects(r);
+        let xs = s.intersect(r);
         assert_eq!(xs[0].object, s);
         assert_eq!(xs[1].object, s);
     }
